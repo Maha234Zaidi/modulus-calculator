70dcc70a08c2aa04ff264feb1ce55f02d488b27c
Maha_Zaidi <mraaj130@gmail.com>
2024-03-15 03:46:09 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+//Only Modulus Calculator
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+const input1 = await inquirer.prompt({
+    name: "number1",
+    type: "number",
+    message :"Enter your first number."
+});
+const input2 = await inquirer.prompt({
+    name :"number2",
+    type: "number",
+    message: "Enter your second number."
+})
+
+const result:number = input1.number1 % input2.number2;
+console.log(`${chalk.bgWhite.blue.bold("Your answer is:")} ${chalk.red.bold.italic.bgWhite(result)}`)
